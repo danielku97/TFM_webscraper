cc5cc83f6aa5e74b58b4ed62e995524ab4f5fdf4
Daniel Ku <danielku97@gmail.com>
2020-12-19 19:02:27 -0500

    Updated xpaths and set scroll timer to 60 seconds

---
+++ scroll_bot.js
@@ -0,0 +1,23 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+async function scroll(){
+	for (i = 0; i < scrollTime; i++){
+		window.scrollTo(0,document.body.scrollHeight);
+		await sleep(500)
+	}
+}
+
+scrollTime = 120;
+
+// customTime = document.getElementById("scrollTime").innerHTML;
+// CustomTime gets passed in from scrollBot.js google chrome file thing
+// if (customTime != null) {
+// 	scrollTime = customTime;
+// }
+
+alert("Scrolling to the bottom (" + scrollTime/2 + " seconds)");
+
+scroll();
